e3e95a9c99419f617150ee42bba79180abce0d2d
kyamada-z <k.yamada@zprs.co.jp>
2026-05-17 18:46:06 +0900

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/CS_insen_17/', // 👈 これを追加することで、GitHub Pagesの階層に対応させます
+  base: '/SalesForecast/', // 👈 今回のリポジトリ名「SalesForecast」に書き換えます
 })
