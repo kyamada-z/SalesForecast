a18263b2e13eed816c3ebc0fb9812b7b44c2927d
kyamada-z <k.yamada@zprs.co.jp>
2026-05-17 19:34:31 +0900

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/SalesForecast/', // 👈 今回のリポジトリ名「SalesForecast」に書き換えます
+  base: '/Sales_Haken_simu/', // 👈 今回のリポジトリ名「SalesForecast」に書き換えます
 })
